c3029a24f3e5c9294135c240a41edbfcaf72b2de
wastingchief <wastingchief@live.com>
2016-03-09 08:02:48 -0800

    Update TCP_Client.py

--- TCP_Client.py
+++ TCP_Client.py
@@ -1,6 +1,7 @@
 #! usr/bin/python3
 
 import socket
+import re 
 
 
 #TCP Client 
@@ -9,18 +10,15 @@ import socket
 ###The server is always expecting us to send data first 
 ####The server is always sending data back in a timely fashion
 
-
 # We will start asking who is our target
 ##target = input("What is your IPv4 target hostname?") 
 ##target_port = input("What port will you be connecting on?") 
 
 
 #Example of input
-#target = www.google.com
-#target_port = 80
-
-
-
+target_host = "www.google.com"
+target_port = 80
+recv_port
 #Creating a socket object 
 #"how our data will be transmitted"
 #AF_INET = We will be using a standard IPv4 address or hostname
@@ -30,27 +28,16 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 #Connecting to the client 
-
-
-client.connect((target, target_prot))
+client.connect((target_host, target_prot))
 
 
 #Send your data[transmission]
-
-
 #Example "Get HTTP version 1.1" 
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
-
-
-#data = input("What data are we send?")
-
-
-#client.send("{}".format(data))
-
-
+#sending string in bytes format to ommit error ive recived 
+send = client.send(bytes("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"))
 
 #Recive a response 
-response = client.recv(4096)
+response = client.recv(recv_port)
 
 print(response)
 
